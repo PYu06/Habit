11cb1ddea0bc8cc9c5900f3734f478401fdd8f4f
Paul Yu <paulyu292@gmail.com>
2015-01-28 15:06:16 -0800

    add edit habit name

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,21 @@
 get '/' do
 	@habits = Habit.all
+	# @habits.each do |habit| @habit = habit end
 	erb :index
 end
 
 post '/habits' do
-	habit = Habit.create(params[:habit])
+	@habit = Habit.create(params[:habit])
 	redirect '/'
+end
+
+get '/habits/:id/edit' do
+	@habit = Habit.find(params[:id])
+	erb :'habits/edit'
+end
+
+put '/habits/:id' do
+	@habit = Habit.find(params[:id])
+	@habit.update_attributes(params[:habit])
+	redirect '/'	
 end
