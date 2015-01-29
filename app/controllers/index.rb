6cdb678476a24ff8e21d129da87ab5fc06e2a995
Paul Yu <paulyu292@gmail.com>
2015-01-29 06:34:15 -0800

    add imgs

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,5 @@
 get '/' do
 	@habits = Habit.all
-	# @habits.each do |habit| @habit = habit end
 	erb :index
 end
 
@@ -30,3 +29,19 @@ get '/habits/:id/done' do
 	@habit.update_all_counters
 	redirect '/'
 end
+
+get '/quote' do
+	response = HTTParty.post("http://api.icndb.com/jokes/random")
+	# one = JSON.parse(response.body)
+	# p response.quoteText
+	# p one["value"]["joke"]
+	response.to_json
+	# ["value"]["joke"]
+
+
+end
+
+# # meh, dunno if want to use
+# get '/quotetwo' do
+# 	p response = HTTParty.get("http://api.theysaidso.com/qod.json")
+# end
