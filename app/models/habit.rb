129fa716a1e35bb7b3e2d483796690de9a8481b4
Paul Yu <paulyu292@gmail.com>
2015-01-28 16:04:00 -0800

    add logic to make it reset to 1 if past 24 hours

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,18 +1,23 @@
 class Habit < ActiveRecord::Base
-  
+	DAY_IN_SECONDS = 86400
+	TEST_TIME = 1
+
+	  
 	def update_all_counters
 		update_daily_counter
 		update_all_time_counter
 	end
 
-	def update_daily_counter
-		p "daily"
-		self.daily_counter += 1
-		self.save
+	def update_all_time_counter
+		if (Time.now.utc - self.updated_at) < DAY_IN_SECONDS
+			self.daily_counter += 1
+			self.save
+		else
+			self.update(daily_counter: 1)
+		end
 	end
 
-	def update_all_time_counter
-		p "consecutive_counter"
+	def update_daily_counter
 		self.consecutive_counter += 1
 		self.save
 	end
